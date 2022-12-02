aa9d91ff25c2cde76f248061d4032aef1e83848b
Cesartwothousands <cesarchen616@outlook.com>
2022-12-02 17:07:44 -0500

    Rename

--- AdjacencyMatrixHungarian_test.py
+++ AdjacencyMatrixHungarian_test.py
@@ -4,9 +4,9 @@ from AdjacencyMatrixHungarian import AdjacencyMatrixHungarian
 
 costEdges=np.array([
     [2,3,2,4],
-        [4,1,5,1],
-        [1,3,6,2],
-        [5,6,7,8]
+    [4,1,5,1],
+    [1,3,6,2],
+    [5,6,7,8]
 ])
 algorithm=AdjacencyMatrixHungarian(costEdges)
 result=algorithm.calculate()
--- hungarian_graph_test.py
+++ hungarian_graph_test.py
@@ -3,9 +3,9 @@ from hungarian_graph import hungarian_graph
 
 costEdges=np.array([
     [2,3,2,4],
-        [4,1,5,1],
-        [1,3,6,2],
-        [5,6,7,8]
+    [4,1,5,1],
+    [1,3,6,2],
+    [5,6,7,8]
 ])
 algorithm=hungarian_graph(costEdges)
 print(algorithm.calculate())
